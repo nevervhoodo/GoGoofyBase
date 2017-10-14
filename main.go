1d5c541da6be873aa09a30c3cf110fc172c160e2
Dina <dkhunter10@gmail.com>
2017-10-14 18:43:36 +0300

    splitted1

--- main.go
+++ main.go
@@ -45,44 +45,6 @@ func check(e error) {
 	}
 }
 
-//Table initialization realisation and declaration
-func initTable()(Table){
-	strs, err := File2lines(dbpath)
-	if err != nil{
-		fmt.Print("db error")
-		table := Table{
-			Field{ID: 0, Key: "test", Value: "testme"},
-		}
-		var str string
-		str = fmt.Sprintf(format+"\n",0,"test","testme")
-		ioutil.WriteFile(dbpath, []byte(str), 0644)
-		return table
-	}
-	if len(strs) == 0{
-		table := Table{
-			Field{ID: 0, Key: "test", Value: "testme"},
-		}
-		var str string
-		str = fmt.Sprintf(format+"\n",0,"test","testme")
-		ioutil.WriteFile(dbpath, []byte(str), 0644)
-		return table
-	}
-	var i,id int
-	var key,val string
-	var table Table
-	for i=0; i<len(strs); i++{
-		//var validID = regexp.MustCompile(`"^([a-z]+): (['a'-'z'],['A'-'Z']+)=(['a'-'z'],['A'-'Z']+)$"`)
-		n, err := fmt.Sscanf(strs[i], format, &id, &key, &val)
-		fmt.Println(strs[i])
-		//fmt.Println(validID.MatchString(strs[i]))
-		if n < 3 || err!= nil{
-			fmt.Printf("problems %d: %s = %s\n", id, val)
-		}
-		table = append(table,Field{ID: id, Key: key, Value: val})
-	}
-	return table
-}
-var mytable Table = initTable()
 
 //Find record in table by its id
 func (table Table) searchById(id int)(Field, bool){
